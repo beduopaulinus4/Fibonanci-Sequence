f4c0499fa1a5fdb96c7f8e1cd59e29280f9d5651
beduopaulinus4 <beduopaulinus4@gmail.com>
2025-11-09 12:02:35 +0000

    Add fibonacci_loop function to generate sequence
    Implement a function to generate Fibonacci numbers.

---
+++ fibonaci.py
@@ -0,0 +1,12 @@
+def fibonacci_loop(n: int):
+    """ Return the first n fibonacci numbers (starting with 0)"""
+    if n <= 0:  # type: ignore
+        return []
+    if n == 1:
+        return [0]
+    seq = [0, 1]
+    for i in range(2, n):
+        seq.append(seq[-1] + seq[-2])
+    return seq
+#call the function to print the first 10 fibonanci numbers
+print(fibonacci_loop(20))
